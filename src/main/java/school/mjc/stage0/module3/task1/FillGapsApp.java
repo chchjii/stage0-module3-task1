c3d89f79125278dba1e5c2bd169882a74f98f1db
Kirill <cryt.mem@gmail.com>
2025-08-11 16:17:38 +0300

    added solution

--- src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
@@ -1,7 +1,9 @@
 package school.mjc.stage0.module3.task1;
 
+import school.mjc.parser.predicate.SoutPredicate;
+
 public class FillGapsApp {
     public static void main(String[] args) {
-              .        ("Fixed");
+        System.out.println("Fixed");
     }
 }
